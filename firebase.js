caa1d3d2f7e7a8d809ebebe4f85be7e099e00881
mhdhssn <mhdhssn967@gmail.com>
2025-07-25 13:09:28 +0530

    firebase key update

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
-const serviceAccount = require('./firebaseKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);;
 
 if (!admin.apps.length) {
   admin.initializeApp({
